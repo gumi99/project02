23ddb18e390e50b48852ce94cf26e9e56e10b0f8
gumi99 <yewongumi@gmail.com>
2024-05-03 15:18:39 +0900

    제출

--- js/common.js
+++ js/common.js
@@ -1,1 +1,24 @@
-$(document).ready(function(){});
+$(document).ready(function(){//시작
+
+  $("header .inner .ham-wrap").click(function(){
+    $(this).toggleClass('active');
+    $("header .inner h1").toggleClass('active');
+    $("nav").slideToggle('show');
+  });
+
+  $(".m-menu > li").click(function(){
+
+    if($(window).width() < 767){
+      if($(this).children(".depth2").css("display")=="block"){
+      $(this).children(".depth2").slideUp();
+      $(this).children(".arr").find("span").removeClass("move");
+    }else{
+      $(".depth2").stop().slideUp();
+      $(this).children(".depth2").slideDown();
+      $(".arr span").removeClass("move");
+      $(this).children(".arr").find("span").addClass("move");
+    };
+    }
+    
+  });
+});//끝

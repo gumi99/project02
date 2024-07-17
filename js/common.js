71c11f9abacfc88415d23c5e33b81f6e09b18a93
gumi99 <168617410+gumi99@users.noreply.github.com>
2024-07-17 22:06:08 +0900

    제출

--- js/common.js
+++ js/common.js
@@ -1,25 +1,25 @@
-$(document).ready(function(){//시작
+$(document).ready(function () {//시작
 
-  $("header .inner .ham-wrap").click(function(){
+  $("header .inner .ham-wrap").click(function () {
     $(this).toggleClass('active');
     $("header .inner h1").toggleClass('active');
     $("nav").slideToggle('show');
   });
 
-  $(".m-menu > li").click(function(){
+  $(".m-menu > li").click(function () {
 
-    if($(window).width() < 767){
-      if($(this).children(".depth2").css("display")=="block"){
-      $(this).children(".depth2").slideUp();
-      $(this).children(".arr").find("span").removeClass("move");
-    }else{
-      $(".depth2").stop().slideUp();
-      $(this).children(".depth2").slideDown();
-      $(".arr span").removeClass("move");
-      $(this).children(".arr").find("span").addClass("move");
-    };
+    if ($(window).width() < 767) {
+      if ($(this).children(".depth2").css("display") == "block") {
+        $(this).children(".depth2").slideUp();
+        $(this).children(".arr").find("span").removeClass("move");
+      } else {
+        $(".depth2").stop().slideUp();
+        $(this).children(".depth2").slideDown();
+        $(".arr span").removeClass("move");
+        $(this).children(".arr").find("span").addClass("move");
+      };
     }
-    
+
   });
 
   AOS.init();
@@ -33,10 +33,10 @@ $(document).ready(function(){//시작
       el: ".swiper-pagination",
       clickable: true
     },
-    slidesPerView : 2,
-    centeredSlides : true ,
-    speed : 1000,
-    loop : true 
+    slidesPerView: 2,
+    centeredSlides: true,
+    speed: 1000,
+    loop: true
   });
 
 });//끝
